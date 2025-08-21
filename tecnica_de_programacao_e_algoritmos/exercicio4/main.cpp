99db2f350df9b30e51f9f3a9de973810cc28a6f7
Nicoli <nicaaracoll@gmail.com>
2025-08-21 14:48:39 -0300

    identação do código 4

--- tecnica_de_programacao_e_algoritmos/exercicio4/main.cpp
+++ tecnica_de_programacao_e_algoritmos/exercicio4/main.cpp
@@ -3,5 +3,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	float a, b, c;
+	char r;
+	r = 's';
+		while(r == 's' || r == 'S'){
+			std:: cout<<"qual o primeiro monomio dos seus calculos? ";
+			std:: cin>>a;
+			std:: cout<<"qual o segundo monomio dos seus calculos? ";
+			std:: cin>>b;
+			c = a + b;
+			std:: cout<< a<< " + "<< b<< " = "<< c<<"\n";
+			c = a - b;
+			std:: cout<< a<< " - "<< b<< " = "<< c<<"\n";
+			c = a * b;
+			std:: cout<< a<< " x "<< b<< " = "<< c<<"\n";
+			c = a / b;
+			std:: cout<< a<< " / "<< b<< " = "<< c<<"\n";
+			std:: cout<<"deseja continuar s/n? ";
+			std:: cin>>r;
+		}
 	return 0;
 }
