b10a6be075932e4d08aee18caaebb4743d301d5b
Douglas <Douglasteles011@gmail.com>
2025-08-28 15:01:35 -0300

    alteração código 4

--- tecnica_de_programacao_e_algoritmos/exercicio4/main.cpp
+++ tecnica_de_programacao_e_algoritmos/exercicio4/main.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+int soma(int x, int y){
+	int z = 0;
+	z = x + y;
+}
+int subtracao(int x, int y){
+	int z = 0;
+	z = x - y;
+}
+int mult(int x, int y){
+	int z = 0;
+	z = x * y;
+}
+int div(int x, int y){
+	float z = 0;
+	z = x / y;
+}
 int main(int argc, char** argv) {
-	float a, b, c;
+	int x, y;
+	float resultado;
 	char r;
 	r = 's';
-		while(r == 's' || r == 'S'){
-			std:: cout<<"qual o primeiro monomio dos seus calculos? ";
-			std:: cin>>a;
-			std:: cout<<"qual o segundo monomio dos seus calculos? ";
-			std:: cin>>b;
-			c = a + b;
-			std:: cout<< a<< " + "<< b<< " = "<< c<<"\n";
-			c = a - b;
-			std:: cout<< a<< " - "<< b<< " = "<< c<<"\n";
-			c = a * b;
-			std:: cout<< a<< " x "<< b<< " = "<< c<<"\n";
-			c = a / b;
-			std:: cout<< a<< " / "<< b<< " = "<< c<<"\n";
-			std:: cout<<"deseja continuar s/n? ";
-			std:: cin>>r;
-		}
+	while(r == 's' || r == 'S'){
+		std::cout << "qual seu primeiro numero? ";
+		std::cin >> x;
+		std::cout << "qual seu segundo numero? ";
+		std::cin >> y;
+		resultado = soma(x, y);
+		std::cout << x << " + " << y << " = " << resultado;
+		resultado = subtracao(x, y);
+		std::cout << "\n" << x << " - " << y << " = " << resultado;
+		resultado = mult(x, y);
+		std::cout << "\n" << x << " X " << y << " = " << resultado;
+		resultado = div(x, y);
+		std::cout << "\n" << x << " : " << y << " = " << resultado;
+		std::cout << "\n deseja continuar s/n? ";
+		std::cin >> r; 
+	}
 	return 0;
 }
